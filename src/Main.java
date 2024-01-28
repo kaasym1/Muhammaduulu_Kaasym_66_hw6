8ae0d20db7a0e1b6cb6084e8bf7b670e70b287bc
Kaasym <152964684+kaasym1@users.noreply.github.com>
2024-01-28 20:32:02 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Boss boss = new Boss();
+        boss.bossWeapon.setWeaponType(WeaponType.BOMB);
+        boss.bossWeapon.setWeaponName("Atom");
+        boss.setHealth(570);
+        boss.setDamage(99);
+        System.out.println("Weapon type: " + boss.bossWeapon.getWeaponType() +
+                "\nWeapon name: " + boss.bossWeapon.getWeaponName() +
+                "\nHealth: " + boss.getHealth() + "\nDamage:" + boss.getDamage());
     }
 }
