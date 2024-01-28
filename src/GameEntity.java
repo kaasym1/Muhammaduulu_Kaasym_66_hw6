0fab72c857f8613c6e9a29fa1af902dd6385de78
Kaasym <152964684+kaasym1@users.noreply.github.com>
2024-01-28 20:31:37 +0600

    Update GameEntity.java

--- src/GameEntity.java
+++ src/GameEntity.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class GameEntity {
+public class GameEntity {
+    int Health;
+    int Damage;
+
+    public int getHealth() {
+        return Health;
+    }
+
+    public void setHealth(int health) {
+        Health = health;
+    }
+
+    public int getDamage() {
+        return Damage;
+    }
+
+    public void setDamage(int damage) {
+        Damage = damage;
+    }
 }
